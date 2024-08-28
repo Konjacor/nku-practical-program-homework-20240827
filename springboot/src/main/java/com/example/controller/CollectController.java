675263b9d20a4197001484674173b28d9e5ff928
Konjacor <weichuanru@126.com>
2024-08-28 16:19:39 +0800

    完善收藏功能前端操作接口

--- springboot/src/main/java/com/example/controller/CollectController.java
+++ springboot/src/main/java/com/example/controller/CollectController.java
@@ -5,11 +5,16 @@ import com.example.entity.Collect;
 import com.example.entity.Help;
 import com.example.service.CollectService;
 import com.github.pagehelper.PageInfo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
 
+@Api(tags = "收藏前端操作接口")
 @RestController
 @RequestMapping("/collect")
 public class CollectController {
@@ -18,6 +23,8 @@ public class CollectController {
     CollectService collectService;
 
     @PostMapping("/add")
+    @ApiImplicitParam(name = "collect",value = "收藏信息",required = true)
+    @ApiOperation(value = "新增收藏信息")
     public Result add(@RequestBody Collect collect) {
         collectService.add(collect);
         return Result.success();
@@ -27,6 +34,8 @@ public class CollectController {
      * 删除
      */
     @DeleteMapping("/delete/{id}")
+    @ApiImplicitParam(name = "id",value = "收藏信息id",required = true)
+    @ApiOperation(value = "根据id删除收藏信息")
     public Result deleteById(@PathVariable Integer id) {
         collectService.deleteById(id);
         return Result.success();
@@ -36,6 +45,8 @@ public class CollectController {
      * 批量删除
      */
     @DeleteMapping("/delete/batch")
+    @ApiImplicitParam(name = "ids",value = "多个收藏信息id",required = true)
+    @ApiOperation(value = "根据id批量删除收藏信息")
     public Result deleteBatch(@RequestBody List<Integer> ids) {
         collectService.deleteBatch(ids);
         return Result.success();
@@ -45,6 +56,11 @@ public class CollectController {
      * 分页查询
      */
     @GetMapping("/selectPage")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "pageNum",value = "当前页数",required = true),
+            @ApiImplicitParam(name = "pageSize",value = "每页记录数",required = true)
+    })
+    @ApiOperation(value = "分页查询所有的收藏信息")
     public Result selectPage(@RequestParam(defaultValue = "1") Integer pageNum,
                              @RequestParam(defaultValue = "10") Integer pageSize) {
         PageInfo<Collect> page = collectService.selectPage(pageNum, pageSize);
