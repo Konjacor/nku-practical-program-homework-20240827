5068a3eebf8b683070a7697d5abb3e04ed52afe6
xzjcoder <xpeiyu@126.com>
2024-08-28 21:39:34 +0800

    收藏功能实现

--- springboot/src/main/java/com/example/controller/LikesController.java
+++ springboot/src/main/java/com/example/controller/LikesController.java
@@ -3,6 +3,9 @@ package com.example.controller;
 import com.example.common.Result;
 import com.example.entity.Likes;
 import com.example.service.LikesService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
+@Api(tags = "收藏操作接口")
 @RestController
 @RequestMapping("/likes")
 public class LikesController {
@@ -18,6 +22,8 @@ public class LikesController {
     LikesService likesService;
 
     @PostMapping("/add")
+    @ApiImplicitParam(name = "likes",value = "收藏信息增加",required = true)
+    @ApiOperation(value = "收藏信息增加")
     public Result add(@RequestBody Likes likes) {
         likesService.add(likes);
         return Result.success();
