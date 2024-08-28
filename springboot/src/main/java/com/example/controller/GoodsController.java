0c9d6d2f959a78c33632d73c88014a1d3d6b1e23
xzjcoder <xpeiyu@126.com>
2024-08-28 21:35:17 +0800

    商品信息增删改查功能实现

--- springboot/src/main/java/com/example/controller/GoodsController.java
+++ springboot/src/main/java/com/example/controller/GoodsController.java
@@ -4,6 +4,10 @@ import com.example.common.Result;
 import com.example.entity.Goods;
 import com.example.service.GoodsService;
 import com.github.pagehelper.PageInfo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import java.util.List;
@@ -11,6 +15,7 @@ import java.util.List;
 /**
  * 二手商品前端操作接口
  **/
+@Api(tags = "商品信息操作接口")
 @RestController
 @RequestMapping("/goods")
 public class GoodsController {
@@ -22,6 +27,8 @@ public class GoodsController {
      * 新增
      */
     @PostMapping("/add")
+    @ApiImplicitParam(name = "goods",value="商品信息",required = true)
+    @ApiOperation(value = "增加评论信息")
     public Result add(@RequestBody Goods goods) {
         goodsService.add(goods);
         return Result.success();
@@ -31,6 +38,8 @@ public class GoodsController {
      * 删除
      */
     @DeleteMapping("/delete/{id}")
+    @ApiImplicitParam(name = "id",value="商品信息id",required = true)
+    @ApiOperation(value = "商品信息删除")
     public Result deleteById(@PathVariable Integer id) {
         goodsService.deleteById(id);
         return Result.success();
@@ -40,6 +49,8 @@ public class GoodsController {
      * 批量删除
      */
     @DeleteMapping("/delete/batch")
+    @ApiImplicitParam(name = "ids",value = "多个商品信息id",required = true)
+    @ApiOperation(value = "根据id批量删除商品信息")
     public Result deleteBatch(@RequestBody List<Integer> ids) {
         goodsService.deleteBatch(ids);
         return Result.success();
@@ -49,12 +60,16 @@ public class GoodsController {
      * 修改
      */
     @PutMapping("/update")
+    @ApiImplicitParam(name = "goods",value = "评论信息修改",required = true)
+    @ApiOperation(value = "评论信息修改")
     public Result updateById(@RequestBody Goods goods) {
         goodsService.updateById(goods);
         return Result.success();
     }
 
     @PutMapping("/updateReadCount/{id}")
+    @ApiImplicitParam(name = "id",value = "评论信息数量修改",required = true)
+    @ApiOperation(value = "评论信息数量修改")
     public Result updateReadCount(@PathVariable Integer id) {
         goodsService.updateReadCount(id);
         return Result.success();
@@ -64,6 +79,8 @@ public class GoodsController {
      * 根据ID查询
      */
     @GetMapping("/selectById/{id}")
+    @ApiImplicitParam(name = "id",value = "商品信息id",required = true)
+    @ApiOperation(value = "商品信息id")
     public Result selectById(@PathVariable Integer id) {
         Goods goods = goodsService.selectById(id);
         return Result.success(goods);
@@ -73,6 +90,8 @@ public class GoodsController {
      * 查询所有
      */
     @GetMapping("/selectAll")
+    @ApiImplicitParam(name = "goods",value = "查询条件",required = false)
+    @ApiOperation(value = "查询商品信息")
     public Result selectAll(Goods goods) {
         List<Goods> list = goodsService.selectAll(goods);
         return Result.success(list);
@@ -82,6 +101,12 @@ public class GoodsController {
      * 分页查询
      */
     @GetMapping("/selectPage")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name="goods", value = "查询条件", required = false),
+            @ApiImplicitParam(name = "pageNum",value = "当前页数",required = true),
+            @ApiImplicitParam(name = "pageSize",value = "每页记录数",required = true)
+    })
+    @ApiOperation(value = "分页查询所有商品信息")
     public Result selectPage(Goods goods,
                              @RequestParam(defaultValue = "1") Integer pageNum,
                              @RequestParam(defaultValue = "10") Integer pageSize) {
@@ -93,6 +118,12 @@ public class GoodsController {
      * 前台分页查询
      */
     @GetMapping("/selectFrontPage")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name="goods", value = "查询条件", required = false),
+            @ApiImplicitParam(name = "pageNum",value = "当前页数",required = true),
+            @ApiImplicitParam(name = "pageSize",value = "每页记录数",required = true)
+    })
+    @ApiOperation(value = "前台分页查询所有商品信息")
     public Result selectFrontPage(Goods goods,
                              @RequestParam(defaultValue = "1") Integer pageNum,
                              @RequestParam(defaultValue = "10") Integer pageSize) {
